72987e393024ebaf6ca0b104268d3ecd4cf980c9
Erica Rabinovich <ericar13@live.com>
2018-03-10 17:54:16 -0500

    Root Motion Implemented

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(CapsuleCollider))]
+public class Hit : MonoBehaviour {
+
+    private Animator anim;
+    private CapsuleCollider collider;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+
+        if (anim == null) {
+            Debug.LogError("Animator not found");
+        }
+
+        collider = GetComponent<CapsuleCollider>();
+
+        if (collider == null) {
+            Debug.LogError("Capsule Collider not found");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Projectiles")) {
+            // play hit animation
+            anim.SetTrigger("hit");
+
+            // change CapsuleCollider to trigger, non longer an obstacle
+            collider.isTrigger = true;
+        }
+    }
+}
--- Assets/Scripts/RootMotionControl.cs
+++ Assets/Scripts/RootMotionControl.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class RootMotionControl : MonoBehaviour {
 
+    public Rigidbody projectile;
+
     private Animator anim;
+    private Transform handHold;
+    private Rigidbody currProjectile;
 
     void Awake() {
+        handHold = this.transform.Find("mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:RightShoulder/" +
+            "mixamorig:RightArm/mixamorig:RightForeArm/mixamorig:RightHand/ProjectileHoldSpot");
+
+        if (projectile == null) {
+            Debug.LogError("projectile has not been assigned a value.");
+        }
+
         anim = GetComponent<Animator>();
 
         if (anim == null) {
@@ -17,17 +28,54 @@ public class RootMotionControl : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+        // initial thing to throw
+        currProjectile = Instantiate(projectile, handHold);
+        currProjectile.isKinematic = true;
+        currProjectile.transform.localPosition = Vector3.zero;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        // move
-        Move();
-	}
-
-    void Move() {
         anim.SetFloat("velx", Input.GetAxis("Horizontal"));
         anim.SetFloat("vely", Input.GetAxis("Vertical"));
-    } 
+    }
+
+    private void FixedUpdate() {
+        if (currProjectile != null && Input.GetButtonDown("Fire1")) {
+            anim.SetBool("throw", true);
+        } else {
+            anim.SetBool("throw", false);
+        }
+    }
+
+    private void Throw() {
+        // release the thing
+        currProjectile.transform.SetParent(null);
+        // make thing under force control
+        currProjectile.isKinematic = false;
+
+        currProjectile.velocity = Vector3.zero;
+        currProjectile.angularVelocity = Vector3.zero;
+
+        // add force to the thing to actually throw it
+        currProjectile.AddForce(this.transform.forward * 13f, ForceMode.VelocityChange);
+
+        // Destroy thing you threw after 2 seconds
+        Destroy(GameObject.Find("ThrowableBall(Clone)"), 2.0f);
+
+        // make sure you can receive another thing
+        currProjectile = null;
+
+        // get a new thing to throw after two-thirds of a second
+        Invoke("Reload", 0.75f);
+
+        
+    }
+
+    private void Reload() {
+        // a new thing to throw
+        currProjectile = Instantiate(projectile, handHold);
+        currProjectile.isKinematic = true;
+        currProjectile.transform.localPosition = Vector3.zero;
+    }
 }
