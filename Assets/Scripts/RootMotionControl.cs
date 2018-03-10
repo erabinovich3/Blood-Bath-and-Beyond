503eb42c4b3bb3b7a384e15f68d8192a1b25e440
Erica Rabinovich <ericar13@live.com>
2018-03-10 13:51:05 -0500

    Working on implementing Root Motion

---
+++ Assets/Scripts/RootMotionControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RootMotionControl : MonoBehaviour {
+
+    private Animator anim;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+
+        if (anim == null) {
+            Debug.LogError("Animator not found");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // move
+        Move();
+	}
+
+    void Move() {
+        anim.SetFloat("velx", Input.GetAxis("Horizontal"));
+        anim.SetFloat("vely", Input.GetAxis("Vertical"));
+    } 
+}
