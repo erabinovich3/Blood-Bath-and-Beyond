e56db0924c7d20d53d0e30cb84c73a0b3539453e
aakanksha1998 <aakanksha.patel1998@gmail.com>
2018-03-28 02:50:53 -0400

    Select mom feature
    This is the feature that allows you to select the mom you want to play
    with. It’s a little slow and has no locks on the moms yet. We will fix
    that in a later milestone though.

---
+++ Assets/Scripts/DisplaySelectedMom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplaySelectedMom : MonoBehaviour {
+  
+	public GameObject[] moms;
+
+	// Use this for initialization
+	void Start () {
+		GameObject temp = GameObject.Find("MomSelect");
+		SelectMom momScript = temp.GetComponent<SelectMom>();
+		int sM = momScript.momNumber;
+		for (int i = 0; i < 5; i++) {
+			if (i == sM) {
+				moms [i].SetActive (true);
+			} else {
+				moms [i].SetActive(false);
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SelectMom.cs
@@ -0,0 +1,65 @@
+﻿	using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMom : MonoBehaviour {
+
+	public GameObject[] moms;
+	public Light[] lights;
+	public int momNumber = 0;
+	public GameObject momSelected;
+
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+//		GameObject[] cmListObj = SceneManager.GetSceneByName ("choose-mom").GetRootGameObjects ();
+//		GameObject temp = cmListObj [1];
+//		for (int i = 0; i < cmListObj.Length; i++) {
+//			temp = cmListObj [i];
+//			Debug.Log (temp.ToString ());
+//		}
+//
+//
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			momNumber = (momNumber + 4) % 5;
+			string logMessage = "The number is " + momNumber + ".";
+			Debug.Log(logMessage);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			momNumber = (momNumber + 1) % 5;
+			string logMessage = "The number is " + momNumber + ".";
+			Debug.Log(logMessage);
+		}
+		GUIChanges ();
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			SelectTheMom ();
+		}
+
+
+	}
+
+	public void SelectTheMom () {
+		//TODO: switch on the mom kinda like toggle button from M1
+		momSelected = moms[momNumber];
+		SceneManager.LoadScene ("level1");
+	}
+
+	public void GUIChanges () {
+		for (int i = 0; i < 5; i++) {
+			if (i == momNumber) {
+				lights [i].intensity = 14;
+			} else {
+				lights [i].intensity = 7;
+			}
+		}
+	}
+}
