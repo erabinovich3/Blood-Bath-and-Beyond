55afb3963338451ee8adfe43836992e52c7416c9
Xuran <xchen459@gatech.edu>
2018-04-05 02:22:23 -0400

    fixed weapon swapping

--- Assets/Scripts/RootMotionControl.cs
+++ Assets/Scripts/RootMotionControl.cs
@@ -5,24 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class RootMotionControl : MonoBehaviour {
 
-    public Rigidbody projectile;
+    
 
     private Animator anim;
-    private Transform handHold;
-    public Rigidbody currProjectile;
+    
+   
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
     public float speedH = 2.0f;
 
     void Awake() {
-        handHold = this.transform.Find("mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:RightShoulder/" +
-            "mixamorig:RightArm/mixamorig:RightForeArm/mixamorig:RightHand/ProjectileHoldSpot");
-
-        if (projectile == null) {
-            Debug.LogError("projectile has not been assigned a value.");
-        }
-
         anim = GetComponent<Animator>();
 
         if (anim == null) {
@@ -30,11 +23,6 @@ public class RootMotionControl : MonoBehaviour {
         }
     }
 
-    // Use this for initialization
-    void Start () {
-        // initial thing to throw
-        Reload();
-    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -53,42 +41,5 @@ public class RootMotionControl : MonoBehaviour {
         
     }
 
-    private void FixedUpdate() {
-        if (currProjectile != null && Input.GetButtonDown("Fire1")) {
-            anim.SetBool("throw", true);
-        } else {
-            anim.SetBool("throw", false);
-        }
-    }
 
-    private void Throw() {
-        // release the thing
-        currProjectile.transform.SetParent(null);
-        // make thing under force control
-        currProjectile.isKinematic = false;
-
-        currProjectile.velocity = Vector3.zero;
-        currProjectile.angularVelocity = Vector3.zero;
-
-        // add force to the thing to actually throw it
-        currProjectile.AddForce(this.transform.forward * 60f, ForceMode.VelocityChange);
-
-        // Destroy thing you threw after 2 seconds
-        Destroy(GameObject.Find("ThrowableBall(Clone)"), 2.0f);
-
-        // make sure you can receive another thing
-        currProjectile = null;
-
-        // get a new thing to throw after two-thirds of a second
-        Invoke("Reload", 0.75f);
-
-        
-    }
-
-    public void Reload() {
-        // a new thing to throw
-        currProjectile = Instantiate(projectile, handHold);
-        currProjectile.isKinematic = true;
-        currProjectile.transform.localPosition = Vector3.zero;
-    }
 }
--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,28 +4,98 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour {
     public Rigidbody[] weaponList;
+    public float reloadCD = 5;
+    public Rigidbody projectile;
+    public Rigidbody currProjectile;
 
+    private Transform handHold;
+    private Animator anim;
     RootMotionControl movement;
     int curIndex = 0;
-    // Use this for initialization
-    void Start () {
-        movement = GetComponent<RootMotionControl>();
+    private float reloadTimer;
+
+    void Awake () {
+
+        handHold = this.transform.Find("mixamorig:Hips/mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:RightShoulder/" +
+            "mixamorig:RightArm/mixamorig:RightForeArm/mixamorig:RightHand/ProjectileHoldSpot");
+
+        projectile = weaponList[0];
+        if (projectile == null)
+        {
+            Debug.LogError("projectile has not been assigned a value.");
+        }
+        
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (weaponList.Length > 1 && Input.GetButtonDown("Fire3")) {
-            Debug.Log("clicked");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        // initial thing to throw
+        Reload();
+    }
+
+
+    private void FixedUpdate()
+    {
+        reloadTimer += Time.deltaTime;
+        if (currProjectile != null && Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("throw");
+        }
+        else
+        {
+            //anim.SetBool("throw", false);
+        }
+        if (weaponList.Length > 1 && Input.GetButtonDown("Fire2") && reloadTimer >= reloadCD)
+        {
+            reloadTimer = 0f;
             curIndex++;
-            if (curIndex == weaponList.Length)
+            if (curIndex >= weaponList.Length)
             {
                 curIndex = 0;
             }
-            Destroy(movement.currProjectile);
-            movement.projectile = weaponList[curIndex];
-            movement.Reload();
+            if (currProjectile != null ) {
+                Destroy(currProjectile.gameObject);
+            }
             
-        }
 
+            //Debug.Log(currProjectile);
+            projectile = weaponList[curIndex];
+            Invoke("Reload", 0.75f);
+
+        }
+    }
+
+    private void Throw()
+    {
+        // release the thing
+        currProjectile.transform.SetParent(null);
+        // make thing under force control
+        currProjectile.isKinematic = false;
+
+        currProjectile.velocity = Vector3.zero;
+        currProjectile.angularVelocity = Vector3.zero;
+
+        // add force to the thing to actually throw it
+        currProjectile.AddForce(this.transform.forward * 60f, ForceMode.VelocityChange);
+
+        // Destroy thing you threw after 2 seconds
+        Destroy(currProjectile.gameObject,  2.0f);
+
+        // make sure you can receive another thing
+        currProjectile = null;
+
+        // get a new thing to throw after two-thirds of a second
+        Invoke("Reload", 0.75f);
+
+
+    }
+
+    public void Reload()
+    {
+        // a new thing to throw
+        currProjectile = Instantiate(projectile, handHold);
+        currProjectile.isKinematic = true;
+        currProjectile.transform.localPosition = Vector3.zero;
     }
 }
