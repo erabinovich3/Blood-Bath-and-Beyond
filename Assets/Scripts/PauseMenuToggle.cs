5c5917f76997031fb2d59e5d00bce4f205a08fb5
vivienneqie <qievi@gatech.edu>
2018-05-01 22:04:11 -0400

    fixed in-game menu

--- Assets/Scripts/PauseMenuToggle.cs
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -4,84 +4,91 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class PauseMenuToggle : MonoBehaviour {
-	
+
+	private CanvasGroup canvasGroup;
+
+	public Button resume;
 	public Button instB;
 	public Button quitB;
-	public Button retuB;
-	private RawImage instrImage;
-	float timer = 0;
-
-	bool paused;
-	bool showingInstructions;
-
-    void LockCursor()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    void UnlockCursor()
-    {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-    }
+	public RawImage instrImage;
+	public Button back;
 
     // Use this for initialization
     void Start () {
 
-        paused = false;
-		showingInstructions = true;
 		instB.onClick.AddListener (showInstructions);
 		quitB.onClick.AddListener (quitGame);
-		retuB.onClick.AddListener (returnToSelectMom);
+		back.onClick.AddListener (goBack);
+		resume.onClick.AddListener (resumeGame);
 
 
 		instB.gameObject.SetActive(false);
 		quitB.gameObject.SetActive(false);
-		retuB.gameObject.SetActive(false);
+		back.gameObject.SetActive(false);
+		resume.gameObject.SetActive (false);
 
-
-		instrImage = GameObject.Find("RawImage").GetComponent<RawImage>();
-		instrImage.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			if (showingInstructions) {
-				instrImage.gameObject.SetActive(false);
-				showingInstructions = false;
-			} else if (paused) {
+			if (canvasGroup.interactable) {
+				Time.timeScale = 1f;
 				instB.gameObject.SetActive(false);
 				quitB.gameObject.SetActive(false);
-				retuB.gameObject.SetActive(false);
-				paused = false;
-                LockCursor();
-                Time.timeScale = 1f;
-
+				resume.gameObject.SetActive (false);
+				canvasGroup.interactable = false;
+				canvasGroup.blocksRaycasts = false;
+				canvasGroup.alpha = 0f;
+				instrImage.gameObject.SetActive(false);
+				Cursor.visible = false;
 			} else {
+				Time.timeScale = 0f;
 				instB.gameObject.SetActive(true);
 				quitB.gameObject.SetActive(true);
-				retuB.gameObject.SetActive(true);
-				paused = true;
-                UnlockCursor();
-				Time.timeScale = 0f;
+				resume.gameObject.SetActive (true);
+				canvasGroup.interactable = true;
+				canvasGroup.blocksRaycasts = true;
+				canvasGroup.alpha = 1f;
+				Cursor.visible = true;
 			}
 		}
 	}
 
 	public void showInstructions() {
 		instrImage.gameObject.SetActive(true);
-		showingInstructions = true;
-	}
-
-	public void returnToSelectMom() {
-		SceneManager.LoadScene("chooseMom");
+		back.gameObject.SetActive (true);
 	}
 
 	public void quitGame() {
 		Application.Quit ();
 	}
 
+	public void goBack() {
+		instrImage.gameObject.SetActive (false);
+		back.gameObject.SetActive (false);
+	}
+
+	public void resumeGame() {
+		Time.timeScale = 1f;
+		instB.gameObject.SetActive(false);
+		quitB.gameObject.SetActive(false);
+		resume.gameObject.SetActive (false);
+		canvasGroup.interactable = false;
+		canvasGroup.blocksRaycasts = false;
+		canvasGroup.alpha = 0f;
+		instrImage.gameObject.SetActive(false);
+		Cursor.visible = false;
+	}
+
+	void Awake () {
+		canvasGroup = GetComponent<CanvasGroup> ();
+
+		if (canvasGroup == null)
+			Debug.LogError ("Canvas group not found");
+
+		Time.timeScale = 1f;
+	}
 }
